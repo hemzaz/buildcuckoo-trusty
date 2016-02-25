f957b58da148885e8f4ea5f13bbacf55178f0d20
hemzaz <elad140@gmail.com>
2016-02-25 08:07:20 -0800

    moved to a new space

--- moloch_add_user.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-cd /data/moloch/viewer
-echo "please enter username :" read user
-echo "please enter password :" read pass
-node addUser.js user "user" pass
-cd ..
--- moloch_init.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-cd /data/moloch/db
-./db.pl 127.0.0.1:9200 init
-cd ..
